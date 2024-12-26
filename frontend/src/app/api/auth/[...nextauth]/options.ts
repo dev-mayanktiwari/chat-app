db06d56ba545beef8d15333eb60efb1e67173ec7
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-27 00:26:15 +0530

    feat: Added auth options

---
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,23 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: AuthOptions = {
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+};
+
+export default authOptions;
