2aa59ffa4971d392055212527e3280fa942d1e17
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-26 23:44:57 +0530

    feat: Initial setup

---
+++ backend/src/config/config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const _config = {
+  PORT: process.env.PORT,
+};
+
+export const config = {
+  get(key: string): string {
+    //@ts-ignore
+    const value = _config[key];
+    if (!value) {
+      console.log(`Error in accessing ${key} value`);
+      process.exit(1);
+    }
+    //@ts-ignore
+    return _config[key];
+  },
+};
