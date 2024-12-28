a3e58e4c2e1e18975083a31204ed610a2ca13902
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-29 00:23:17 +0530

    feat: Updated footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-white">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+    <footer className="bg-white border-t border-gray-200">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 md:flex md:items-center md:justify-between">
         <div className="flex justify-center space-x-6 md:order-2">
           <Link href="/privacy" className="text-gray-400 hover:text-gray-500">
             Privacy Policy
