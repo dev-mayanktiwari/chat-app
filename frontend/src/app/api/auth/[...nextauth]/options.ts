ae68e78334e2aeea2e36336b6875a530bc707b98
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-28 11:22:39 +0530

    feat: Added auth callbacks

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,10 @@
 import { AuthOptions, ISODateString } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 
 export interface CustomSession {
   user?: CustomUser;
-  expires?: ISODateString;
+  expires: ISODateString;
 }
 
 export interface CustomUser {
@@ -18,6 +19,31 @@ export interface CustomUser {
 export const authOptions: AuthOptions = {
   pages: {
     signIn: "/",
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      console.log("The user data is", user);
+      console.log("The account data is", account);
+      return true;
+    },
+    async session({
+      session,
+      user,
+      token,
+    }: {
+      session: CustomSession;
+      user: CustomUser;
+      token: JWT;
+    }) {
+      session.user = token.user as CustomUser;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
   },
   providers: [
     GoogleProvider({
