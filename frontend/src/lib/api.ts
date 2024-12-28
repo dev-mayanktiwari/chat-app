a363b68e4dcd77ae09046db573cb2950f3de9565
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-28 19:35:09 +0530

    feat: Updated APIs

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,11 @@ import { LOGIN_URL } from "./apiEndpoints";
 
 export const login = (payload: any) => {
   try {
-    const response = axios.post(LOGIN_URL, payload);
+    const response = axios.post(LOGIN_URL, payload, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return response;
   } catch (error) {
     console.error("Login error:", error);
--- frontend/src/lib/apiEndpoints.ts
+++ frontend/src/lib/apiEndpoints.ts
@@ -1,5 +1,7 @@
 import Env from "./env";
 
 export const BASE_URL = Env.BACKEND_URL;
-export const API_URL = `${BASE_URL}/API_URL`;
-export const LOGIN_URL = `${API_URL}/auth/login`;
+export const API_URL = Env.API_URL;
+
+const FULL_BASE_URL = `${BASE_URL}/${API_URL}`;
+export const LOGIN_URL = `${FULL_BASE_URL}/auth/login`; 
