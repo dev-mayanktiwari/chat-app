6d4265685e579a5cc37c26880b790c7c2fa8f0da
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-30 22:30:54 +0530

    feat: Added token decoder

--- backend/src/services/quicker.ts
+++ backend/src/services/quicker.ts
@@ -1,5 +1,11 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload, verify } from "jsonwebtoken";
 import { config } from "../config/config";
+
+interface AuthenticatedUser {
+  name: string;
+  email: string;
+  id: string;
+}
 
 export default {
   createToken: (data: any) => {
@@ -7,5 +13,19 @@ export default {
       expiresIn: "30d",
     });
     return token;
+  },
+
+  verifyToken: (token: string): AuthenticatedUser => {
+    const decoded = verify(token, config.get("JWT_SECRET"));
+    if (typeof decoded !== "object" || !decoded) {
+      throw new Error("Invalid token");
+    }
+
+    const { name, email, id } = decoded as JwtPayload;
+    if (!name || !email || !id) {
+      throw new Error("Invalid token payload");
+    }
+
+    return { name, email, id };
   },
 };
