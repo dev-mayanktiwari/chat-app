d3e422ea2bb4f2beda09c402222d22b820866991
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-31 18:53:05 +0530

    feat: Added get chatroom db

--- backend/src/services/chatRoomDBServices.ts
+++ backend/src/services/chatRoomDBServices.ts
@@ -2,11 +2,23 @@ import prisma from "../config/db.config";
 
 export default {
   createChatroom: (userId: number, title: string, passcode: string) => {
-    prisma.chatGroup.create({
+    console.log("inside query");
+    return prisma.chatGroup.create({
       data: {
         title,
         passcode,
         userId,
+      },
+    });
+  },
+
+  getChatrooms: (userId: number) => {
+    return prisma.chatGroup.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createdAt: "desc",
       },
     });
   },
